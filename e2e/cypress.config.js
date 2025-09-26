9bbefcf0df65af0dfe5a3558602664c95606e883
aekkasit <aekkasit.sak@student.mahidol.edu>
2025-09-26 20:35:19 +0700

    No dockerfile

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     e2e: {
         baseUrl: "http://localhost:3000",
         supportFile: "cypress/support/e2e.js",
+        specPattern: "cypress/e2e/**/*.cy.js",
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
